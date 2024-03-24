45b0365bed3c1a19afe27f27465654f4f4ce88dd
vedarth31 <vedarth31@gmail.com>
2024-03-23 20:47:39 -0500

    added linkedin icon and links to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 import Logo from '../../assets/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faYoutube, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faYoutube, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './footer.css'
 
 export default function Footer(){
@@ -13,17 +13,20 @@ export default function Footer(){
 
         <div className="footer-right-container">
             <div className="socials-container">
-                <a rel="noopener noreferrer">
+                <a rel="noopener noreferrer" className="icon" href="https://www.instagram.com/tamuformulaelectric/">
                     <FontAwesomeIcon icon={faInstagram}/>
                 </a>
-                <a rel="noopener noreferrer" className='youtube'>
+                <a rel="noopener noreferrer" className='icon' href="https://www.youtube.com/@tamuformulaelectric">
                     <FontAwesomeIcon icon={faYoutube}/>
                 </a>
-                <a rel="noopener noreferrer">
+                <a rel="noopener noreferrer" className="icon" href="https://github.com/TAMU-Formula-SAE-EV"> 
                     <FontAwesomeIcon icon={faGithub}/>
                 </a>
+                <a rel="noopener noreferrer" className="icon" href="https://www.linkedin.com/company/texas-a-m-fsae-electric-racing/"> 
+                    <FontAwesomeIcon icon={faLinkedin}/>
+                </a>
             </div>
-            <h1 className='contact'>Contact</h1>
+            <h1 className='contact'> <a href="mailto:tamuformulaelectric@gmail.com">Contact</a></h1>
         </div>
     </footer>
 }
