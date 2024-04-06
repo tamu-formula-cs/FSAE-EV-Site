30b2d0395641c297e798cdf2f712c2b0810b708c
barivadar <bari.vadaria@gmail.com>
2024-04-06 18:13:19 -0500

    updated become sponsor page

--- src/pages/sponsors/becomeSponsors.jsx
+++ src/pages/sponsors/becomeSponsors.jsx
@@ -17,129 +17,131 @@ function SponsorshipTiers() {
       <Header />
       <div className="sponsorship-tiers">
         <h1 className="sponsors-title">SPONSORSHIP TIERS</h1>
-        <table className="sponsorship-tiers-table">
-          <thead>
-            <tr>
-              <th className="normal-text">OUR COMMITMENT TO YOU</th>
-                <th className="tier-header">
-                  <span className="affiliate">AFFILIATE</span><br/>
-                  <span className="amount-text">Up to $1,500</span>
-                </th>
-                <th className="tier-header">
-                  <span className="bronze">BRONZE</span><br/>
-                  <span className="amount-text">$1,500+</span>
-                </th>
-                <th className="tier-header">
-                  <span className="silver">SILVER</span><br/>
-                  <span className="amount-text">$3,500+</span>
-                </th>
-                <th className="tier-header">
-                  <span className="gold">GOLD</span><br/>
-                  <span className="amount-text">$5,000+</span>
-                </th>
-                <th className="tier-header">
-                  <span className="platinum">PLATINUM</span><br/>
-                  <span className="amount-text">$8,000+</span>
-                </th>
-                <th className="tier-header">
-                  <span className="title-text">TITLE</span><br/>
-                  <span className="amount-text">$17,500</span>
-                </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="normal-text">501(3)(c) TAX BENEFITS</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">INVITATION TO UNVEILING EVENT</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">HONORED ON OUR WEBSITE</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">LOGO ON TEAM APPAREL</td>
-              <td className="logo-text">SMALL</td>
-              <td className="logo-text">SMALL</td>
-              <td className="logo-text">SMALL</td>
-              <td className="logo-text">MEDIUM</td>
-              <td className="logo-text">LARGE</td>
-              <td className="logo-text">X-LARGE</td>
-            </tr>
-            <tr>
-              <td className="normal-text">SOCIAL MEDIA EXPOSURE</td>
-              <td></td>
-              <td></td>
-              <td className="logo-text">x1</td>
-              <td className="logo-text">x2</td>
-              <td className="logo-text">x2</td>
-              <td className="logo-text">x3</td>
-            </tr>
-            <tr>
-              <td className="normal-text">LOGO ON RACECAR</td>
-              <td></td>
-              <td></td>
-              <td className="logo-text">SMALL</td>
-              <td className="logo-text">MEDIUM</td>
-              <td className="logo-text">LARGE</td>
-              <td className="logo-text">X-LARGE</td>
-            </tr>
-            <tr>
-              <td className="normal-text">HOST RECRUITING EVENTS</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">ACCESS TO TEAM RESUME BOOK</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td className="checkmark">✔</td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">BRANDED BANNER</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td className="checkmark">✔</td>
-            </tr>
-            <tr>
-              <td className="normal-text">SAE DINNER EVENT</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td className="checkmark">✔</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="sponsorship-tiers-table-container">
+          <table className="sponsorship-tiers-table">
+            <thead>
+              <tr>
+                <th className="normal-text">OUR COMMITMENT TO YOU</th>
+                  <th className="tier-header">
+                    <span className="affiliate">AFFILIATE</span><br/>
+                    <span className="amount-text">Up to $1,500</span>
+                  </th>
+                  <th className="tier-header">
+                    <span className="bronze">BRONZE</span><br/>
+                    <span className="amount-text">$1,500+</span>
+                  </th>
+                  <th className="tier-header">
+                    <span className="silver">SILVER</span><br/>
+                    <span className="amount-text">$3,500+</span>
+                  </th>
+                  <th className="tier-header">
+                    <span className="gold">GOLD</span><br/>
+                    <span className="amount-text">$5,000+</span>
+                  </th>
+                  <th className="tier-header">
+                    <span className="platinum">PLATINUM</span><br/>
+                    <span className="amount-text">$8,000+</span>
+                  </th>
+                  <th className="tier-header">
+                    <span className="title-text">TITLE</span><br/>
+                    <span className="amount-text">$17,500</span>
+                  </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="normal-text">501(3)(c) TAX BENEFITS</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">INVITATION TO UNVEILING EVENT</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">HONORED ON OUR WEBSITE</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">LOGO ON TEAM APPAREL</td>
+                <td className="logo-text">SMALL</td>
+                <td className="logo-text">SMALL</td>
+                <td className="logo-text">SMALL</td>
+                <td className="logo-text">MEDIUM</td>
+                <td className="logo-text">LARGE</td>
+                <td className="logo-text">X-LARGE</td>
+              </tr>
+              <tr>
+                <td className="normal-text">SOCIAL MEDIA EXPOSURE</td>
+                <td></td>
+                <td></td>
+                <td className="logo-text">x1</td>
+                <td className="logo-text">x2</td>
+                <td className="logo-text">x2</td>
+                <td className="logo-text">x3</td>
+              </tr>
+              <tr>
+                <td className="normal-text">LOGO ON RACECAR</td>
+                <td></td>
+                <td></td>
+                <td className="logo-text">SMALL</td>
+                <td className="logo-text">MEDIUM</td>
+                <td className="logo-text">LARGE</td>
+                <td className="logo-text">X-LARGE</td>
+              </tr>
+              <tr>
+                <td className="normal-text">HOST RECRUITING EVENTS</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">ACCESS TO TEAM RESUME BOOK</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="checkmark">✔</td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">BRANDED BANNER</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="checkmark">✔</td>
+              </tr>
+              <tr>
+                <td className="normal-text">SAE DINNER EVENT</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="checkmark">✔</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <div className="button-container">
           <button className="become-sponsor-button" onClick={handleSponsorClick}>BECOME A SPONSOR</button>
           <button className="become-sponsor-button" onClick={handleMoreInfoClick}>MORE INFORMATION</button>
