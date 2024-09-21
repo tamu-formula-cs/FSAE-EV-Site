595822149481ae9bd650cbb6347a8b8426dfc0cf
vedarth31 <vedarth31@gmail.com>
2024-09-21 14:06:52 -0500

    contact form update

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import { useState } from "react";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
     const [thankYouMessageVisible, setThankYouMessageVisible] = useState(false);
@@ -8,19 +9,19 @@ export default function Contact() {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        const form = event.target;
-        const formData = new FormData(form);
+        const service_id = import.meta.env.VITE_EMAIL_SERVICE_ID
+        const template_id = import.meta.env.VITE_EMAIL_TEMPLATE_ID;
+        const api_key = import.meta.env.VITE_EMAIL_API_KEY;
 
         try {
-            const response = await fetch(form.action, {
-                method: 'POST',
-                body: formData,
-                mode: 'no-cors'
-            });
+            const result = await emailjs.sendForm(
+                service_id,
+                template_id,
+                event.target,
+                api_key
+            );
 
-            console.log('Form submitted successfully', response);
-
-            form.reset();
+            event.target.reset();
             setThankYouMessageVisible(true);
 
             setTimeout(() => {
@@ -47,24 +48,23 @@ export default function Contact() {
                 <h1 className="text-black mt-20 mb-0" style={{ fontSize: '5rem' }}>Contact Us</h1>
                 <form
                     className="mt-4 flex flex-col items-center w-4/5 mx-auto relative"
-                    action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScIDzc4swg6AYECqs3vnjOvldE3cwpr29QLivPrr0SVa5FJNw/formResponse?pli=1"
                     onSubmit={handleSubmit}
                 >
                     <input
                         className="mb-2 p-2 w-1/2 border border-maroon rounded-md"
                         type="text"
-                        name="entry.276066810"
+                        name="user_name"
                         placeholder="Your Name"
                     />
                     <input
                         className="mb-2 p-2 w-1/2 border border-maroon rounded-md"
                         type="email"
-                        name="entry.1504943651"
+                        name="user_email"
                         placeholder="Your Email"
                     />
                     <textarea
                         className="mb-12 p-2 w-1/2 h-48 border border-maroon rounded-md"
-                        name="entry.412394112"
+                        name="message"
                         placeholder="Your Message"
                     />
                     <div className="absolute bottom-0">
@@ -85,5 +85,5 @@ export default function Contact() {
             <div className="mt-20"></div>
             <Footer />
         </>
-    )
+    );
 }
