ab648ca549ca475509aca26630b7b7b1c4c26e90
Athul Suresh <athulrsuresh@gmail.com>
2025-02-07 10:56:07 -0600

    seo shi (hopefully this dont break things)

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+export default async function sitemap() {
+    const baseUrl = 'https://tamuformulaelectric.com';
+    
+    return [
+      {
+        url: baseUrl,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/ame22`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/ame23`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/ame24`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/ame25`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/get-involved`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/meet-the-team`,
+        lastModified: new Date(),
+      },
+    ]
+  }
